a3f47e46fbd03b4ef1bbaacb0f737255a79461f9
Augustvilliam <willyp@hotmail.se>
2024-12-19 22:54:38 +0100

    skojade bara. mer tester.

---
+++ Business.Tests/HelperTest/IdGeneratorTests.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Business.Helper;
+
+namespace Business.Tests.HelperTest;
+
+public class IdGeneratorTests
+{
+    [Fact]
+    public void GenerateShorIdShoudReturnTrimmedId()
+    {
+        //act
+        var id = IdGenerator.GenerateShortId(5);
+        //assert
+        Assert.NotNull(id);
+        Assert.Equal(5, id.Length);
+    }
+
+    [Fact]
+    public void GenerateShortIdShouldThrowExWhenLengthIsOutOfBound()
+    {
+        Assert.Throws<ArgumentException>(() => IdGenerator.GenerateShortId(0));
+        Assert.Throws<ArgumentException>(() => IdGenerator.GenerateShortId(33));
+    }
+}
---
+++ Business.Tests/ServicesTest/UserManagementServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Tests.ServicesTest;
+
+public class UserManagementServiceTests
+{
+    [Fact]
+    public void ShowMenuShouldReturnToMainMenuIfSelected()
+    {
+    }
+    [Fact]
+     public void CreateUserShouldReturnValidUser()
+    {
+    }
+
+
+}
