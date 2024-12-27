e734d2a68708246f5a6f4a9f8cc4d5d8e09e4550
Augustvilliam <willyp@hotmail.se>
2024-12-27 14:29:20 +0100

    Påbörja DI. Följa upp Att jag följer SOLID med chatGPT.

--- Console.MainApp/Program.cs
+++ Console.MainApp/Program.cs
@@ -1,14 +1,26 @@
-﻿using Business.Interfaces;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Business.Interfaces;
 using Business.Services;
 
-IFileService fileService = new FileService();
-IUserService userService = new UserService(fileService);
-var importExportService = new ImportExportService(userService, fileService);
-var userManagementService = new UserManagementService(userService);
-var mainMenuService = new MainMenuService(userManagementService, importExportService);
 
+
+var serviceCollection = new ServiceCollection();
+
+serviceCollection.AddTransient<IUserService, UserService>();
+
+var serviceProvider = serviceCollection.BuildServiceProvider ();
+
+
+
+//*
+//*IFileService fileService = new FileService();
+//*IUserService userService = new UserService(fileService);
+//*var importExportService = new ImportExportService(userService, fileService);
+//*//*var userManagementService = new UserManagementService(userService);
+//*var mainMenuService = new MainMenuService(userManagementService, importExportService);
+//*
 // Visa huvudmenyn
-mainMenuService.ShowMenu();
+//*mainMenuService.ShowMenu();
 
 
 
